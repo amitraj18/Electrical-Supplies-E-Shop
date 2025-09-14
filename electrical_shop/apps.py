bb1e680d11bbefc37c8adeca49ef7de8ccb0b858
amitraj18 <amitrajpadaki@gmail.com>
2025-09-14 13:02:26 +0530

    Update apps.py

--- electrical_shop/apps.py
+++ electrical_shop/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 class ElectricalShopConfig(AppConfig):
-         default_auto_field = "django.db.models.BigAutoField"
-         name = "electrical_shop"
+      default_auto_field = "django.db.models.BigAutoField"
+      name = "electrical_shop"
+
 
